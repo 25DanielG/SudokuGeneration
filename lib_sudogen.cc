42a16ea5218ca1d7a6c05d8bd295e18de7cf9da1
NitrusYT1 <78567926+NitrusYT1@users.noreply.github.com>
2022-06-13 09:02:18 -0700

    Added Sudoku Gen Files
    Just added Sudo Solver files

---
+++ lib_sudogen.cc
@@ -0,0 +1,10 @@
+#include "./sudogen.h"
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::pair;
+
+
---
+++ sudogen.h
@@ -0,0 +1,13 @@
+#ifndef SUDOGEN_H_
+#define SUDOGEN_H_
+#include <vector>
+using std::vector;
+
+namespace sudoGen {
+
+  int grid[9][3];   
+}
+
+
+
+#endif
