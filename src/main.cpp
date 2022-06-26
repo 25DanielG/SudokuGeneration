af35c63d1504bf9e7ad58528c4ff594b01133915
25DanielG <93798575+25DanielG@users.noreply.github.com>
2022-06-25 17:27:31 -0700

    generation stuck in infinite loop

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ using std::pair;
 // cd src
 // g++ lib_sudosolver.cc main.cpp -o sudoSolver (compile command)
 // ./sudoSolver (exec command)
-bool solvable(vector<vector<int>> &board, const int boardSize) {
+bool solvable(vector<vector<int> > &board, const int boardSize) {
     return (sudoSolver::solveSudoku(boardSize, board));
 }
 int main() {
@@ -22,13 +22,25 @@ int main() {
        return 0;
     }
     vector<vector<int> > board(boardSize, vector<int>(boardSize, 0));
+    cout << solvable(board, boardSize) << endl;
     while(!solvable(board, boardSize)) {
-        std::default_random_engine gen; // generates the random numbers to constantly make a different sudoku
+        std::random_device ranDevice; // generates the random numbers to constantly make a different sudoku
+        std::mt19937 gen(ranDevice());
         std::uniform_int_distribution<int> boardDist(0, boardSize - 1);
         std::uniform_int_distribution<int> insertDist(1, 9);
-        int ranXIndex = boardDist(gen);
-        int ranYIndex = boardDist(gen);
+        int ranRowIndex = boardDist(gen);
+        int ranColIndex = boardDist(gen);
         int insertedNumber = insertDist(gen);
-        board[ranXIndex][ranYIndex] = insertedNumber;
+        if(!sudoSolver::numberInRow(boardSize, board, ranRowIndex, insertedNumber)
+           && !sudoSolver::numberInColumn(boardSize, board, ranColIndex, insertedNumber)
+           && !sudoSolver::numberInBox(board, (ranRowIndex - ranRowIndex % 3), (ranColIndex - ranColIndex % 3), insertedNumber)) {
+            board[ranRowIndex][ranColIndex] = insertedNumber;
+        }
+    }
+    for(int i = 0; i < boardSize; ++i) {
+        for(int j = 0; j < boardSize; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
     }
 }
