afb5dde969a27e9a2e0346b4305207baad47b6fc
25DanielG <93798575+25DanielG@users.noreply.github.com>
2022-08-05 10:06:40 +0300

    hmmmm

---
+++ src/CMakeFiles/sudoSolve.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoSolve.dir/lib_sudosolver.cc.o"
+  "CMakeFiles/sudoSolve.dir/lib_sudosolver.cc.o.d"
+  "libsudoSolve.a"
+  "libsudoSolve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sudoSolve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
